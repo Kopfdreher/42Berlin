5d949a70ae4de1c61372f06a76220da62df038b5
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2025-07-14 19:50:20 +0200

    Finished second variation of gnl

--- medium-gnl/get_next_line.c
+++ medium-gnl/get_next_line.c
@@ -12,18 +12,42 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+void	print_newline_helper(char *buffer)
 {
-	int		bytes_read;
-	char	*buffer;
+	while (buffer && *buffer)
+	{
+		if (*buffer == '\n')
+		{
+			*buffer = '\\';
+		}
+		printf("%c", *buffer);
+		buffer++;
+	}
+}
 
-	buffer = ft_calloc(3 + 1, sizeof(char));
+static char	*read_from_file(int fd)
+{
+	int			bytes_read;
+	char		*buffer;
+	static int	count;
+
+	count = 1;
+	printf("ft_calloc#[%d]---", count++);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!buffer)
 		free(buffer);
-	bytes_read = read(fd, buffer, 3);
-	if (!bytes_read)
-		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	print_newline_helper(buffer);
+	if (bytes_read <= 0)
+		return (free(buffer), NULL);
 	return (buffer);
+}
 
+char	*get_next_line(int fd)
+{
+	char	*basin_buffer;
+
+	basin_buffer = read_from_file(fd);
+	return (basin_buffer);
 }
 
