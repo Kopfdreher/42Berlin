23ad14a2aa18f8b8a5fda182f9598075e01cfc65
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2025-11-20 18:27:57 +0100

    Working on printing stacks

--- Rank2/push_swap/main.c
+++ Rank2/push_swap/main.c
@@ -1,10 +1,12 @@
+/* ************************************************************************** */
+/*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_push_swap.c                                     :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/15 11:41:02 by sgavrilo          #+#    #+#             */
-/*   Updated: 2025/11/20 13:08:59 by sgavrilo         ###   ########.fr       */
+/*   Created: 2025/11/20 18:11:49 by sgavrilo          #+#    #+#             */
+/*   Updated: 2025/11/20 18:25:14 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -12,11 +14,63 @@
 #include "push_swap.h"
 
 void	ft_error();
+void	args_to_stack(char *argv[], t_list **stack);
+void	print_stacks(t_list *a, t_list *b);
 
 int	main(int argc, char *argv[])
 {
+	t_list	*a;
+	t_list	*b;
+
+	a = NULL;
+	b = NULL;
 	if (argc < 3 || input_has_errors(argc, argv))
 		ft_error();
+	args_to_stack(argv, &a);
+	ft_printf("%i", *(int *)(a->content));
+	//print_stacks(a, b);
+
+}
+
+void	print_stacks(t_list *a, t_list *b)
+{
+	int i;
+
+	i = 0;
+	ft_printf("|---a---|  |---b---|\n");
+	while(a || b)
+	{
+		if (a)
+		{
+			ft_printf("|   %i   |  ", *(int *)a->content);
+			a = a->next;
+		}
+		else
+			ft_printf("|        |  ");
+		if (b)
+		{
+			ft_printf("|   %i   |\n", *(int *)b->content);
+			b = b->next;
+		}
+		else
+			ft_printf("|        |\n");
+	}
+}
+void	args_to_stack(char *argv[], t_list **stack)
+{
+	int	i;
+	t_list	*temp;
+	int *content;
+
+	i = 1;
+	while (argv[i])
+	{
+		content = safe_malloc(sizeof(int), *stack);
+		*content = ft_atoi(argv[i]);
+		temp = ft_lstnew(content);
+		ft_lstadd_back(stack, temp);
+		i++;
+	}
 }
 
 void	ft_error()
