189724568c6e522753210aa11063630e7b8c4707
Sergej Gavrilov <sgavrilo@c4c4c1.42berlin.de>
2025-12-09 19:58:07 +0100

    Getting 2nd Cmd running if 1st fails

--- Rank2/pipex/testing-pipes/pipex.c
+++ Rank2/pipex/testing-pipes/pipex.c
@@ -6,7 +6,7 @@
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 13:10:08 by sgavrilo          #+#    #+#             */
-/*   Updated: 2025/12/09 18:32:28 by sgavrilo         ###   ########.fr       */
+/*   Updated: 2025/12/09 19:57:06 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,20 @@
 static int	first_cmd(t_cmds *cmds)
 {
 	char	*path;
-	int		fd_in;
 
-	fd_in = open(cmds->infile, O_RDONLY);
-	if (fd_in == -1)
-		return(perror("pipex"), close(cmds->fd[1]), -1);
 	path = path_finding(*cmds->value[0], cmds->envp);
 	if (!path)
-		return (free(path), -1);
+		return (free(path), close(cmds->fd[1]), -1);
+	if (cmds->in_fd == -1)
+		return (perror(*cmds->value[0]), close(cmds->fd[1]), -1);
 	cmds->id[0] = fork();
 	if (cmds->id[0] == -1)
-		return (free(path), -1);
+		return (free(path), close(cmds->fd[1]), -1);
 	if (cmds->id[0] == 0)
 	{
-		if (dup2(cmds->fd[1], STDOUT_FILENO) == -1)
-			return(free(path), perror("pipex"), -1);
-		close(cmds->fd[1]);
-		close(cmds->fd[0]);
+		dup2(cmds->fd[1], STDOUT_FILENO);
+		dup2(cmds->in_fd, STDIN_FILENO);
+		close_all(cmds);
 		execve(path, cmds->value[0], NULL);
 	}
 	close(cmds->fd[1]);
@@ -42,23 +39,20 @@ static int	first_cmd(t_cmds *cmds)
 static int	second_cmd(t_cmds *cmds)
 {
 	char	*path;
-	int		fd_out;
 
-	fd_out = open(cmds->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_out == -1)
-		return(perror("pipex"), -1);
 	path = path_finding(*cmds->value[1], cmds->envp);
 	if (!path)
-		return (free(path), -1);
+		return (free(path), close(cmds->fd[0]), -1);
+	if (cmds->out_fd == -1)
+		return (perror(*cmds->value[1]), close(cmds->fd[0]), -1);
 	cmds->id[1] = fork();
 	if (cmds->id[1] == -1)
-		return (free(path), -1);
+		return (free(path), close(cmds->fd[0]), -1);
 	if (cmds->id[1] == 0)
 	{
-		if (dup2(cmds->fd[0], STDIN_FILENO) == -1)
-			return(free(path), perror("pipex"), -1);
-		close(cmds->fd[0]);
-		close(cmds->fd[1]);
+		dup2(cmds->fd[0], STDIN_FILENO);
+		dup2(cmds->out_fd, STDOUT_FILENO);
+		close_all(cmds);
 		execve(path, cmds->value[1], NULL);
 	}
 	close(cmds->fd[0]);
@@ -72,7 +66,8 @@ int	main(int argc, char *argv[], char **envp)
 	if (argc == 5)
 	{
 		cmds = NULL;
-		if (initialize(cmds, argv, envp))
+		cmds = initialize(cmds, argv, envp);
+		if (!cmds)
 			return (1);
 		if (first_cmd(cmds))
 			close(cmds->fd[0]);
