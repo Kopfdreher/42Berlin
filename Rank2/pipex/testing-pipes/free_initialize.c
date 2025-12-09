4475b8110a64a00ffc9a0d5208bafa90be7d0cc9
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2025-12-09 19:58:07 +0100

    Getting 2nd Cmd running if 1st fails

--- Rank2/pipex/testing-pipes/free_initialize.c
+++ Rank2/pipex/testing-pipes/free_initialize.c
@@ -6,7 +6,7 @@
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 15:06:55 by sgavrilo          #+#    #+#             */
-/*   Updated: 2025/12/09 18:32:45 by sgavrilo         ###   ########.fr       */
+/*   Updated: 2025/12/09 19:49:45 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,29 @@ void	free_all(t_cmds *cmds)
 	}
 }
 
-int	initialize(t_cmds *cmds, char **argv, char **envp)
+void	close_all(t_cmds *cmds)
+{
+		close(cmds->fd[1]);
+		close(cmds->fd[0]);
+		close(cmds->in_fd);
+		close(cmds->out_fd);
+}
+
+t_cmds	*initialize(t_cmds *cmds, char **argv, char **envp)
 {
 	cmds = malloc(sizeof(t_cmds));
 	if (!cmds)
-		return (free_all(cmds), 1);
-	cmds->infile = argv[1];
-	cmds->outfile = argv[4];
+		return (free_all(cmds), NULL);
+	cmds->in_fd = open(argv[1], O_RDONLY);
+	cmds->out_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	cmds->envp = envp;
 	cmds->value[0] = ft_split(argv[2], ' ');
 	if (!cmds->value[0])
-		return (free_all(cmds), 1);
+		return (free_all(cmds), NULL);
 	cmds->value[1] = ft_split(argv[3], ' ');
 	if (!cmds->value[1])
-		return (free_all(cmds), 1);
+		return (free_all(cmds), NULL);
 	if (pipe(cmds->fd) == -1)
-		return (free_all(cmds), 1);
-	return (0);
+		return (free_all(cmds), NULL);
+	return (cmds);
 }
--- Rank2/pipex/testing-pipes/pipex.h
+++ Rank2/pipex/testing-pipes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:20:41 by sgavrilo          #+#    #+#             */
-/*   Updated: 2025/12/09 17:57:23 by sgavrilo         ###   ########.fr       */
+/*   Updated: 2025/12/09 19:47:39 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 
 typedef struct s_cmds
 {
-	char	*infile;
-	char	*outfile;
+	int		in_fd;
+	int		out_fd;
 	char	**value[2];
 	char	**envp;
 	int		id[2];
@@ -29,6 +29,7 @@ typedef struct s_cmds
 
 char	*path_finding(char *cmd, char **envp);
 void	free_all(t_cmds *cmds);
-int		initialize(t_cmds *cmds, char **argv, char **envp);
+void	close_all(t_cmds *cmds);
+t_cmds	*initialize(t_cmds *cmds, char **argv, char **envp);
 
 #endif
