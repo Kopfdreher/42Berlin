03f1ea2c64f7df0e4e697242c14bf175ee2db541
Sergej Gavrilov <sgavrilo@c4b5c1.42berlin.de>
2025-12-16 14:13:38 +0100

    Working on Movement

--- Rank2/so_long/test/key_press.c
+++ Rank2/so_long/test/key_press.c
@@ -6,46 +6,72 @@
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 19:13:28 by sgavrilo          #+#    #+#             */
-/*   Updated: 2025/12/14 15:32:10 by sgavrilo         ###   ########.fr       */
+/*   Updated: 2025/12/16 14:11:56 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int	key_is_up(int keycode)
+void	move_left(t_map *map)
 {
-	return (keycode == 119 || keycode == 65362);
+	map->content[map->p_y][map->p_x] = '0';
+	map->p_x--;
+	map->content[map->p_y][map->p_x] = 'P';
 }
 
-static int	key_is_right(int keycode)
+static void key_is_left(t_map *map)
 {
-	return (keycode == 100 || keycode == 65363);
+
+	ft_printf("LEFT\n");
+	if (map->content[map->p_y][map->p_x - 1] == '0')
+		move_left(map);
 }
 
-static int	key_is_down(int keycode)
+static void	key_is_up(t_map *map)
 {
-	return (keycode == 115 || keycode == 65364);
+	ft_printf("UP\n");
+	if (map->content[map->p_y - 1][map->p_x] == '0')
+	{
+		map->content[map->p_y][map->p_x] = '0';
+		map->p_y--;
+		map->content[map->p_y][map->p_x] = 'P';
+	}
 }
 
-static int	key_is_left(int keycode)
+static void	key_is_right(t_map *map)
 {
-	return (keycode == 97 || keycode == 65361);
+	ft_printf("RIGHT\n");
+	if (map->content[map->p_y][map->p_x + 1] == '0')
+	{
+	map->content[map->p_y][map->p_x] = '0';
+	map->p_x++;
+	map->content[map->p_y][map->p_x] = 'P';
+	}
+}
+
+static void	key_is_down(t_map *map)
+{
+	ft_printf("DOWN\n");
+	if (map->content[map->p_y + 1][map->p_x] == '0')
+	{
+	map->content[map->p_y][map->p_x] = '0';
+	map->p_y++;
+	map->content[map->p_y][map->p_x] = 'P';
+	}
 }
 
 int	handle_keypress(int keycode, t_mlx *mlx)
 {
 	if (keycode == 65307)
-	{
-		mlx_destroy_window(mlx->mlx, mlx->win);
 		free_exit(*mlx, 0);
-	}
-	if (key_is_left(keycode))
-		ft_printf("LEFT\n");
-	if (key_is_up(keycode))
-		ft_printf("UP\n");
-	if (key_is_right(keycode))
-		ft_printf("RIGHT\n");
-	if (key_is_down(keycode))
-		ft_printf("DOWN\n");
+	if (keycode == 97 || keycode == 65361)
+		key_is_left(&mlx->map);
+	if (keycode == 119 || keycode == 65362)
+		key_is_up(&mlx->map);
+	if (keycode == 100 || keycode == 65363)
+		key_is_right(&mlx->map);
+	if (keycode == 115 || keycode == 65364)
+		key_is_down(&mlx->map);
+	draw_map(mlx);
 	return (0);
 }
