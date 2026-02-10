d71a629dfd0ce9e7f27b7d6e70dff5123eaecf4f
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2026-02-10 18:10:05 +0100

    cpp04 Tutorial

---
+++ Rank4/tutorial/cpp04tut/abstractClassesAndInterfaces/poly2.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#inclde < iostream>
+
+class ACharacter {
+
+public:
+  virtual void attack(std::string const &target) = 0;
+  void sayHell9(std::string const &target);
+};
+
+class Warrior : public ACharacter {
+
+public:
+  virtual void attack(std::string const &target);
+};
+
+void Warrior::attack(std :; string const &target) {
+
+  std::cout << "*attacks " << target << " with a sword*" << std::endl;
+}
+
+class ICoffeeMaker {
+
+public:
+  virtual void fillWaterTank(IWaterSource *src) = 0;
+  virtual ICoffee *makeCoffee(std::string const &type) = 0;
+};
+
+int main() {
+
+  ACharacter *a = new Warrior();
+
+  //This would NOT be ok because ACharacter is abstract
+  //ACharacter *b = new ACharacter();
+
+  a->sayHello("students");
+  a->attack("roger");
+}
