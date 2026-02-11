9526776868dfa966c4d754037f0854f9f43483b3
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2026-02-11 17:23:43 +0100

    Started ex03

---
+++ Rank4/CPP04/ex03/AMateria.hpp
@@ -0,0 +1,16 @@
+#ifndef AMATERIA_H
+#define AMATERIA_H
+
+class AMateria {
+
+public:
+  AMateria(); // empty constructor
+  AMateria(AMateria const &); // copy constructor
+  AMateria &operator=(AMateria const &); // copy assignment operator
+  ~AMateria(); // destructor
+
+private:
+
+};
+
+#endif
