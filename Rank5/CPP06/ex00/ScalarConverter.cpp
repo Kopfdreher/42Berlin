eeb98bdb77d0c53631cbf8bae37f0a745a6e879f
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2026-03-13 21:21:12 +0100

    ex00: work in progress

--- Rank5/CPP06/ex00/ScalarConverter.cpp
+++ Rank5/CPP06/ex00/ScalarConverter.cpp
@@ -1,20 +1,31 @@
 #include "ScalarConverter.hpp"
 #include <cctype>
 #include <iostream>
+#include <stdexcept>
 
 // public ----------------------------------------------------------------------
 
+// Todo: rebuild structure around possibility and printConversion
+// rebuild printType-Functions to convertFromType-Functions
 void ScalarConverter::convert(const std::string &literal) {
   eType type = setType(literal);
+  DataValue value;
+
+  value.isCharPossible = true;
+  value.isIntPossible = true;
+
   switch (type) {
   case PSEUDO:
     std::cout << "PSEUDO" << std::endl;
-    break;
+    printPseudo(literal);
+    return;
   case CHAR:
     std::cout << "CHAR" << std::endl;
+    printChar(literal);
     break;
   case INT:
     std::cout << "INT" << std::endl;
+    printInt(literal);
     break;
   case FLOAT:
     std::cout << "FLOAT" << std::endl;
@@ -23,12 +34,81 @@ void ScalarConverter::convert(const std::string &literal) {
     std::cout << "DOUBLE" << std::endl;
     break;
   default:
-    std::cout << "INVALID" << std::endl;
+    std::cerr << "Error: Invalid-Input" << std::endl;
+    return;
   }
+  setPosibility(value);
+  printConversion(value);
 }
 
-// private
-// ---------------------------------------------------------------------
+// private ---------------------------------------------------------------------
+
+void ScalarConverter::printInt(const std::string &literal) {
+  DataValue value;
+  long tmp = std::strtol(literal.c_str(), NULL, 10);
+
+  if (tmp > INT_MAX || tmp < INT_MIN) {
+    std::cerr << "Error: Invalid-Input" << std::endl;
+    break;
+  }
+  value.i = static_cast<int>(tmp);
+  value.c = static_cast<char>(tmp);
+  value.f = static_cast<float>(tmp);
+  value.d = static_cast<double>(tmp);
+  setPossibility(value);
+  printConversion(value);
+}
+
+void ScalarConverter::printChar(const std::string &literal) {
+  DataValue value;
+
+  value.c = literal[0];
+  value.i = static_cast<int>(value.c);
+  value.f = static_cast<float>(value.c);
+  value.d = static_cast<double>(value.c);
+  setPossibility(value);
+  printConversion(value);
+}
+
+void ScalarConverter::printPseudo(const std::string &literal) {
+  std::string pseudoF[] = {"-inff", "+inff", "nanf"};
+  std::string pseudoD[] = {"-inf", "+inf", "nan"};
+
+  int i = 0;
+  while (i < 6) {
+    if (literal == pseudoF[i] || literal == pseudoD[i])
+      break;
+    i++;
+  }
+  std::cout << "char: impossible\n";
+  std::cout << "int: impossible\n";
+  std::cout << "float: " << pseudoF[i] << "\n";
+  std::cout << "double: " << pseudoD[i] << std::endl;
+}
+
+void ScalarConverter::printConversion(const DataValue &value) {
+  // CHAR
+  if (value.isCharPossible) {
+    if (value.c >= ' ' && value.c <= '~')
+      std::cout << "char: '" << value.c << "'\n";
+    else
+      std::cout << "char: Non displayable\n";
+  } else {
+    std::cout << "char: impossible\n";
+  }
+  // INT
+  if (value.isIntPossible)
+    std::cout << "int: " << value.i << "\n";
+  else
+    std::cout << "int: impossible\n";
+
+  // FLOAT
+  std::cout << "float: " << value.f << "\n";
+  // DOUBLE
+  std::cout << "double: " << value.d << std::endl;
+}
+
+void ScalarConverter::setPossibility(DataValue &value) {}
 
 ScalarConverter::eType ScalarConverter::setType(const std::string &literal) {
   if (isPseudo(literal))
--- Rank5/CPP06/ex00/ScalarConverter.hpp
+++ Rank5/CPP06/ex00/ScalarConverter.hpp
@@ -15,10 +15,27 @@ private:
   operator=(ScalarConverter const &); // copy assignment operator
   ~ScalarConverter();                 // destructor
 
+  // STRUCTS
+  struct DataValue {
+    double d;
+    float f;
+    int i;
+    char c;
+    bool intIsPossible;
+    bool charIsPossible;
+  };
+
   // ENUMS
   enum eType { CHAR, INT, FLOAT, DOUBLE, PSEUDO, INVALID };
 
   // METHODS
+  static void printInt(const std::string &literal);
+  static void printChar(const std::string &literal);
+  static void printPseudo(const std::string &literal);
+  static void printConversion(const DataValue &value);
+
+  static void setPossibility(DataValue &value);
+
   static eType setType(const std::string &literal);
   static bool isPseudo(const std::string &literal);
   static bool isChar(const std::string &literal);
